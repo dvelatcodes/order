4f1cf52d0360760a344119dc96ba65294db4587a
Love Olubummo <olubummolove3@gmail.com>
2023-01-20 11:05:16 +0100

    Updates

---
+++ precache-manifest.dc4fd7d632d1344e3a882003c7b494eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e120a3f5b768b9769b48b390b41cc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "938b62903e409f821883",
+    "url": "/static/css/main.7ca03e99.chunk.css"
+  },
+  {
+    "revision": "47ce0c75ae6eb5a769cb",
+    "url": "/static/js/2.88c56bf4.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.88c56bf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "938b62903e409f821883",
+    "url": "/static/js/main.b5fca6bc.chunk.js"
+  },
+  {
+    "revision": "9fe1e2fc4f608a1af71e",
+    "url": "/static/js/runtime-main.d6463fc5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/orderss/precache-manifest.31a396007a276362ecdf96695b1b76c7.js"
+  "/precache-manifest.dc4fd7d632d1344e3a882003c7b494eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/orderss/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
