7a971427cc4b8982d48b56e17637c53fbebe06bd
Love Olubummo <olubummolove3@gmail.com>
2023-01-20 03:00:52 +0100

    Updates

---
+++ precache-manifest.a484bb5314045d065ba17f4fd6b35e6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473b8be48b6664844f99498b8ead70eb",
+    "url": "/order/index.html"
+  },
+  {
+    "revision": "938b62903e409f821883",
+    "url": "/order/static/css/main.7ca03e99.chunk.css"
+  },
+  {
+    "revision": "47ce0c75ae6eb5a769cb",
+    "url": "/order/static/js/2.88c56bf4.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/order/static/js/2.88c56bf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "938b62903e409f821883",
+    "url": "/order/static/js/main.b5fca6bc.chunk.js"
+  },
+  {
+    "revision": "adc7bb00701dca02273c",
+    "url": "/order/static/js/runtime-main.a4cb207f.js"
+  }
+]);
